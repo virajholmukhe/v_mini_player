cdfe07676c1c328c6f9f5825a894578f20a924b8
Viraj <virajholmukhe@gmail.com>
2021-09-15 17:05:53 +0530

    Ready to deploy

--- V_mini_main/settings.py
+++ V_mini_main/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'V_mini_main.urls'
